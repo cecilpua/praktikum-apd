a60d0d114326e9dde6bae3c97bdfb4c490013c28
cecilia <cecilpua@gmail.com>
2025-10-01 19:44:47 +0800

    Menambahkan File Python

--- post-test/post-test-apd-3/2509106125-CeciliaMarsyaPua-PT-3.py
+++ post-test/post-test-apd-3/2509106125-CeciliaMarsyaPua-PT-3.py
@@ -4,19 +4,13 @@ print("=== Penghitung Gaji Karyawan PT. BOM ===")
 # Input data karyawan
 nama = input("Masukkan nama karyawan: ")
 jabatan = input(f"Masukkan jabatan karyawan (peracik/pengantar): ")
-hari_kerja = int(input("Masukkan jumlah hari kerja: "))
-jam_kerja = int(input("Masukkan jumlah jam kerja per hari: "))
-lembur = int(input("Masukkan jumlah lembur: "))
 
-# Harga petasan per pcs
-harga_petasan = 5000
-
-# Inisialisasi bayaran
-bayaran_per_jam = 0
-bayaran_lembur = 0
 
 # Percabangan utama berdasarkan jabatan
 if jabatan == "peracik":
+    hari_kerja = int(input("Masukkan jumlah hari kerja: "))
+    jam_kerja = int(input("Masukkan jumlah jam kerja per hari: "))
+    lembur = int(input("Masukkan jumlah lembur: "))
     if hari_kerja >= 24 and jam_kerja >= 8 and lembur >= 4:
         bayaran_per_jam = 25000
         bayaran_lembur = 15000
@@ -28,6 +22,9 @@ if jabatan == "peracik":
         bayaran_lembur = 10000
 
 elif jabatan == "pengantar":
+    hari_kerja = int(input("Masukkan jumlah hari kerja: "))
+    jam_kerja = int(input("Masukkan jumlah jam kerja per hari: "))
+    lembur = int(input("Masukkan jumlah lembur: "))
     if hari_kerja >= 20 and jam_kerja >= 7 and lembur >= 7:
         bayaran_per_jam = 25000
         bayaran_lembur = 20000
@@ -39,7 +36,8 @@ elif jabatan == "pengantar":
         bayaran_lembur = 12000
 
 else:
-    print("Jabatan tidak dikenali. Silakan masukkan 'peracik' atau 'pengantar'.")
+    print("Jabatan tidak dikenali")
+    exit()
     
 
 # Perhitungan total gaji
@@ -49,7 +47,6 @@ total_gaji = ((bayaran_per_jam * jam_kerja) * hari_kerja) + (lembur * bayaran_le
 print("\n<=== Rincian Gaji Karyawan ===>")
 print("Nama Karyawan       :", nama)
 print("Jabatan             :", jabatan.capitalize())
-print("Harga Petasan/pcs   : Rp{:,.0f}".format(harga_petasan))
 print("Hari Kerja          :", hari_kerja)
 print("Jam Kerja per Hari  :", jam_kerja)
 print("Jumlah Lembur       :", lembur)
