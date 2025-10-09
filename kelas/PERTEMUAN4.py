d5802705384556f233fb019689f44604de8edd1c
cecilia <cecilpua@gmail.com>
2025-10-09 21:55:51 +0800

    Tugas

---
+++ kelas/PERTEMUAN4.py
@@ -0,0 +1,29 @@
+for i in range(1, 4):
+    print (f'i: {i}')
+    for j in range (1, 5):
+        print(f'i: {i}, j: {j}')
+    print (' ')
+
+for i in range(1, 4):
+    print (f'(i : {i})')
+    for j range (1, 7)
+        print(f'')
+    
+
+jawab = 'ya'
+hitung = 0
+while(jawab == 'ya'):
+hitung += 1
+jawab = input("Ulang lagi? ")
+print(f"Total perulangan: {hitung}")
+
+hitung = 0
+while (True):
+    print(hitung)
+    hitung += 1
+    if hitung % 2 == 10:
+        break
+        continue
+    if hitung 
+print(f"Total Perulangan: {hitung}")
+
